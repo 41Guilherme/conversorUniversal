05a453aa51de8e461ee9cffa167b1cbfff8cca41
41Guilherme <guilherme41@usp.br>
2022-04-15 11:56:43 -0300

    Novas Rotas adcionadas

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import React from 'react';
 import './App.css';
 import { Fragment } from 'react';
 import Page from './components/JS/Page';
+import Temp from './components/JS/Temp';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
 import Money from './components/JS/Money';
+import Pesos from './components/JS/Pesos';
+import Medidas from './components/JS/Medidas';
 
 function App() {
   return (
     <Fragment>
       <Router>
         <Switch>
+        <Route path='/medida'>
+          <Medidas/>
+        </Route>
+        <Route path='/peso'>
+          <Pesos/>
+        </Route>
+        <Route path='/temps'>
+            <Temp/>
+        </Route>
         <Route path='/money'>
             <Money/>
         </Route>
-          <Route path='/'>
-            <Page/>
-          </Route>
+        <Route path='/'>
+          <Page/>
+        </Route>
         </Switch>
       </Router>
     </Fragment>
