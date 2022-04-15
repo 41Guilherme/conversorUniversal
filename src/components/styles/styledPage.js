9ecf838be588e78a30080e7433c34801a858e78a
41Guilherme <guilherme41@usp.br>
2022-04-15 11:56:06 -0300

    Componentização dos Styled Components

--- src/components/styles/styledPage.js
+++ src/components/styles/styledPage.js
@@ -38,18 +38,54 @@ export  const Central = styled.div`
         align-items: center;
         margin-bottom: 2px;
         .value1{
+            display: flex;
+            justify-content: center;
+            text-align: center;
             width: 20rem;
             height: 28rem;
-            background-color: green;
+            background-color: white;
             border: solid 1.5px black;
             border-radius: 25px;
+            flex-direction: column;
+            select{
+                width: 15rem;
+                height: 2.2rem;
+                margin-bottom: 2rem;
+            }
+            hr{
+                width: 18rem;
+            }
+            input{
+                width: 15rem;
+                height: 1.8rem;
+                font-size: 2rem;
+                text-align: center;
+            }
         }
         .value2{
+            display: flex;
+            justify-content: center;
+            text-align: center;
             width: 20rem;
             height: 28rem;
-            background-color: yellow;
+            background-color: white;
             border: solid 1.5px black;
             border-radius: 25px;
+            flex-direction: column;
+            select{
+                width: 15rem;
+                height: 2.2rem;
+                margin-bottom: 2rem;
+            }
+            hr{
+                width: 18rem;
+            }
+            input{
+                width: 15rem;
+                height: 1.8rem;
+                font-size: 2rem;
+                text-align: center;
+            }
         }
         span{
             margin: 0;
@@ -57,7 +93,15 @@ export  const Central = styled.div`
             img{
                 width: 75px;
                 height: 75px;
+                border: solid 1px black;
+                border-radius: 25px;
+                :hover{
+                transform: scale(1.1);
+                box-shadow: inset 0 0 1em black, 0 0 2em green;
+                color: red;
             }
+            }
+            
         }
     }
     footer{
@@ -66,6 +110,19 @@ export  const Central = styled.div`
         button{
             padding: 10px;
             border-radius: 25px;
+            :hover{
+                transform: scale(1.1);
+                box-shadow: inset 0 0 1em black, 0 0 2em green;
+                color: green;
+            
+            }
+            
+            
         }
-    }
-`
+        .back{
+            :hover{
+            transform: scale(1.1);
+            box-shadow: inset 0 0 1em black, 0 0 2em red;
+            color: red;
+        }
+        `
